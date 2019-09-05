8ecc03180c07a285b4c73a464083783ac773e7a1
Hans-Nikolai Viessmann <hans@viess.mn>
2019-09-05 12:13:10 +0200

    add conflicts_with to hwloc

--- Formula/hwloc1.rb
+++ Formula/hwloc1.rb
@@ -5,12 +5,9 @@ class Hwloc1 < Formula
   url "https://www.open-mpi.org/software/hwloc/v1.11/downloads/hwloc-1.11.13.tar.bz2"
   sha256 "a4494b7765f517c0990d1c7f09d98cb87755bb6b841e4e2cbfebca1b14bac9c8"
 
-  #bottle do
-  #  cellar :any
-  #  sha256 "639f5ae402481d9f8ff540e1d5131ba037791c585fbfdc26e58850b00428e46d" => :high_sierra
-  #  sha256 "5ba351fdb5f165f29f00428f5ea429c055a59914e0691577a5e02500c5dc6c21" => :sierra
-  #  sha256 "e3ea9be003573ee1d254c38f2ddf65de8c7d18a4fcedc87545825d796d19341e" => :el_capitan
-  #end
+  revision 1
+
+  conflicts_with "hwloc", :because => "homebrew/core formula uses HEAD, which is the v2 branch of hwloc" 
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
